27c8ac6a1ce241278af8184dfe04d20415fc9c52
Kenneth Reitz <me@kennethreitz.org>
2018-03-02 16:28:32 -0500

    wtf
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- pipenv/resolver.py
+++ pipenv/resolver.py
@@ -10,14 +10,80 @@ for _dir in ('vendor', 'patched', '..'):
 
 import pipenv.utils
 import pipenv.core
+import requests
+from requests_html import HTMLSession
 from docopt import docopt
+from pip.req import InstallRequirement
+
+class Resolver(object):
+    """The Pipenv super–resolver."""
+
+    def __init__(self, sources, project, verbose=False):
+        super(Resolver, self).__init__()
+        self.sources = sources
+        self.project = project
+        self.verbose = verbose
+        self.session = HTMLSession()
+        self.unresolved = {}
+        self.resolved = {}
+
+    def __iter__(self):
+        for package_name, package in self.unresolved.copy().items():
+            del self.unresolved[package_name]
+            yield package_name, package
+
+    def register(self, package):
+        self.unresolved.update(package)
+
+    def fetch_versions(self, package_name, source):
+        url = '{0}/{1}'.format(source['url'], pipenv.utils.pep423_name(package_name))
+        versions = set()
+
+        r = self.session.get(url)
+        for link in r.html.absolute_links:
+            version = link.split('-')[1].split('#')[0]
+            for extension in ('.tar.gz', '.win', '.win32', '.zip'):
+                version = version.replace(extension, '')
+
+            versions.add(version)
+
+        return versions
+
+
+
+
+    def graph(self):
+        for package_name, package_version in self:
+            for source in self.sources:
+                versions = self.fetch_versions(package_name, source)
+
+
+            print(package_name, package_version)
+
+
+            # self.resolved[package_name] = self.unresolved[package_name]
+
+
+
+        return self.resolved
 
 
 def which(*args, **kwargs):
     return sys.executable
 
-def resolve(packages, pre, sources, verbose, clear):
-    return pipenv.utils.resolve_deps(packages, which, project=project, pre=pre, sources=sources, clear=clear, verbose=verbose)
+project = pipenv.core.project
+
+resolver = Resolver(sources=project.sources, project=project)
+
+def resolve(packages, pre, sources, verbose=False, clear=False):
+    packages = [pipenv.utils.convert_deps_from_pip(package) for package in packages]
+
+    for package in packages:
+        resolver.register(package)
+
+    return resolver.graph()
+
+    # return pipenv.utils.resolve_deps(packages, which, project=project, pre=pre, sources=sources, clear=clear, verbose=verbose)
 
 if __name__ == '__main__':
 
@@ -32,8 +98,6 @@ if __name__ == '__main__':
         for i, package in enumerate(packages):
             if package.startswith('--'):
                 del packages[i]
-
-    project = pipenv.core.project
 
     results = resolve(packages, pre=do_pre, sources=project.sources, verbose=is_verbose, clear=do_clear)
 
